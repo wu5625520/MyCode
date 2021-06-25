de4d06b391a14cf7c0622bc64e03e7b86c0f9de3
wxy <936233324@qq.com>
2021-06-25 16:21:28 +0800

    aaaaaa

--- java/test01/src/print.java
+++ java/test01/src/print.java
@@ -2,5 +2,7 @@ public class print {
     public static void main(String[] args){
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("更新3");
+        System.out.println("更新4");
     }
 }
