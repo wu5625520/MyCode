f51e7c419d8bae3e3ab75c5dc850255aeeae4ef6
wxy <936233324@qq.com>
2021-06-25 16:23:11 +0800

    java01更新1

--- java/test01/src/print.java
+++ java/test01/src/print.java
@@ -1,8 +1,6 @@
 public class print {
     public static void main(String[] args){
         System.out.println("更新1");
-        System.out.println("更新2");
-        System.out.println("更新3");
-        System.out.println("更新4");
+
     }
 }
